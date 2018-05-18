5235f4e89956532af03a86ee7eda0dd34650ba28
Milos Dzeletovic <miloshdz@gmail.com>
2018-05-18 16:40:28 +0200

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'; // taking all named exports and importing into firebase variable
 
 const config = {
-  apiKey: "AIzaSyBGz7BuujLB_bUo2Pcfk0Tk__Cnae4O9tw",
-  authDomain: "expensify-52fff.firebaseapp.com",
-  databaseURL: "https://expensify-52fff.firebaseio.com",
-  projectId: "expensify-52fff",
-  storageBucket: "expensify-52fff.appspot.com",
-  messagingSenderId: "1031378404340"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
@@ -14,13 +14,6 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export { firebase, database as default };
-
-
-
-
-
-
-
 
 
 
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -2,10 +2,10 @@
 
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve({
-    //   name: 'Milos',
-    //   age: '30'
-    // });
+    resolve({
+      name: 'Milos',
+      age: '30'
+    });
     reject('Something went wrong!');
   }, 2000);
 });
@@ -14,6 +14,14 @@ console.log('before');
 
 promise.then((data) => {
   console.log('1', data);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 2000);
+  });
+}).then((str) => {
+  console.log('does this run?', str);
 }).catch((error) => {
   console.log('error: ', error);
 });
